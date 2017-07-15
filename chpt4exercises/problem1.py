9d318210970656f7940a9711a8dbfc0008c27837
Justin Choi <justinch@uw.edu>
2017-07-15 16:03:09 -0700

    Added parts of chapter 4 exercises

---
+++ chpt4exercises/problem1.py
@@ -0,0 +1,5 @@
+"""
+Exercise 7: Rewrite the grade program from the previous
+chapter using a function called computegrade that takes
+a score as its parameter and returns a grade as a string
+""".
---
+++ chpt4exercises/questions.py
@@ -0,0 +1,25 @@
+"""
+Exercise 4: What is the purpose of the "def" keyword in Python?
+
+a) It is slang that means "the following code is really cool"
+b) It indicates the start of a function
+c) It indicates that the following indented section of code is to be stored for later
+d) b and c are both true
+e) None of the above
+"""
+
+# Answer: d) b and c are both true
+
+
+# What will the following Python program print out?
+def fred():
+   print("Zap")
+
+def jane():
+   print("ABC")
+
+jane()
+fred()
+jane()
+
+# Answer: "ABC Zap ABC" (Response D)
